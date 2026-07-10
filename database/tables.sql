1c4d26b7d21762acaa8036a0f73ba407fed528bb
abdulmoiz9963 <moiz9963@gmail.com>
2026-07-10 11:44:29 +0500

    add new insertion 2

--- database/tables.sql
+++ database/tables.sql
@@ -1,25 +1,25 @@
 -- Create the table
-CREATE TABLE IF NOT EXISTS employees (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL UNIQUE,
-    department VARCHAR(100),
-    salary NUMERIC(10,2),
-    created_at TIMESTAMP DEFAULT NOW()
-);
+-- CREATE TABLE IF NOT EXISTS employees (
+--     id SERIAL PRIMARY KEY,
+--     name VARCHAR(100) NOT NULL UNIQUE,
+--     department VARCHAR(100),
+--     salary NUMERIC(10,2),
+--     created_at TIMESTAMP DEFAULT NOW()
+-- );
 
 -- Insert sample data (safe to re-run, won't duplicate)
-INSERT INTO employees (name, department, salary) VALUES
-    ('Ali Raza', 'Engineering', 95000),
-    ('Sara Khan', 'Marketing', 72000),
-    ('Bilal Ahmed', 'Engineering', 88000),
-    ('Ayesha Malik', 'Finance', 79000),
-    ('Usman Tariq', 'Sales', 68000),
-    ('Hassan Iqbal', 'Engineering', 91000),
-    ('Zara Sheikh', 'Marketing', 75000),
-    ('Fahad Nasir', 'Finance', 82000) ,
-    ('Abdul Moiz', 'devops', 94000),
-    ('Fouzan', 'aws devops', 94000),
-    ('Awais', 'Software engineer', 194000),
-    ('Fahad', 'Software', 194000)
+INSERT INTO employees (name1, department, salary) VALUES
+    -- ('Ali Raza', 'Engineering', 95000),
+    -- ('Sara Khan', 'Marketing', 72000),
+    -- ('Bilal Ahmed', 'Engineering', 88000),
+    -- ('Ayesha Malik', 'Finance', 79000),
+    -- ('Usman Tariq', 'Sales', 68000),
+    -- ('Hassan Iqbal', 'Engineering', 91000),
+    -- ('Zara Sheikh', 'Marketing', 75000),
+    -- ('Fahad Nasir', 'Finance', 82000) ,
+    -- ('Abdul Moiz', 'devops', 94000),
+    -- ('Fouzan', 'aws devops', 94000),
+    -- ('Awais', 'Software engineer', 194000),
+    ('Fahad1', 'Software', 194000)
 
 ON CONFLICT (name) DO NOTHING;
